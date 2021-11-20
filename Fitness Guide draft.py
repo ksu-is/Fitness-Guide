ff1f3ff1bd8d4bb1f24ef7eb7ac0c4039081cea9
amart12 <89154927+amart12@users.noreply.github.com>
2021-11-20 14:08:31 -0500

    Add files via upload

---
+++ Fitness Guide draft.py
@@ -0,0 +1,53 @@
+print('Welcome to your Fitness Guide!')
+def fitness_guide():
+    equipment = input('Please enter yes or no for equipment')
+    if equipment.lower() = 'yes':
+        target_body : input('Please choose upperbody or lowerbody')
+        if target_body.lower() = 'upperbody':
+            area_p = input('Choose which area:')
+            if area_p.lower() = 'arm':
+                print(arm_equip, '10 - 12 times each exercise, 4 sets')
+            elif area_p.lower() = 'shoulder'
+                print(shoulder_equip, '10 - 12 times exercise, 4 sets')
+            elif area_p.lower() = 'back':
+                print(back_equip, '10 - 15 times each exercise, 4 sets')
+            elif area_p.lower() = 'abs':
+                print(abs_equip, '10 - 12 times each exercise, 4 sets')
+
+        elif target_body.lower() = 'lowerbody':
+            area_p = input('Choose which area:')
+            if area_p.lower() = 'leg':
+                print(leg_equip, '12 - 18 times each exercise, 4 sets')
+            elif area_p.lower() = 'hips':
+                print(hips_equip, '13 - 15 times each exercise, 5 sets')
+            elif area_p.lower() = 'glutes':
+                print(glutes_equip, '15 - 18 times each exercise, 4 sets')
+    elif equipment.lower() = 'no':
+        target_body = input('Please choose upperbody or lowerbody')
+        if target_body.lower() = 'upperbody':
+            area_p = input('Choose which area: ')
+            if area_p.lower() = 'arm':
+                print(arm_ne, ' 12 - 15 times each exercise, 4 sets')
+            elif area_p.lower() = 'shoulder':
+                print(shoulder_ne, '12 - 15 times each exercise, 4 sets')
+            elif area_p.lower() = 'back':
+                print(back_ne, ' 12 - 15 times each exercise, 5 sets')
+            elif area_p.lower() = 'abs':
+                print(abs_ne, ' 13 - 16 times each exercise, 4 sets')
+        if target_body.lower() = 'lowerbody':
+            area_p = input('Choose which area: ')
+            if area_p.lower() = 'leg':
+                print(leg_ne, ' 15 - 20 times each exercise, 5 sets')
+            elif area_p.lower() = 'hips':
+                print(hips_ne, ' 13 - 18 times each exercise, 4 sets')
+            elif area_p.lower() = 'glutes':
+                print(glutes_ne, ' 15 - 18 times each exercise, 4 sets') 
+        
+
+
+
+
+    
+
+        
+
